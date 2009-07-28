de5fa99b5bd963a318224a5c2309ee0cc6e66817
mspatil1061 <mspatil1061@e549a66c-7351-11de-90dc-5d84f995b3ea>
2009-07-28 17:39:16 +0000

    php files for module-poll

---
+++ trunk/module_poll_php/votepoll.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h2>Voting the Poll</h2>
+<form id="form1" name="form1" method="post" action="">
+  <p><strong>Question</strong>: <em>poll question from db </em></p>
+  <p><strong>Options</strong>:</p>
+  <p> 1.
+    <input name="radiobutton" type="radio" value="radiobutton" />
+  <em>option1 from db </em></p>
+  <p>2. 
+    <input name="radiobutton" type="radio" value="radiobutton" />
+    <em>option2 from db</em></p>
+  <p>3. 
+    <input name="radiobutton" type="radio" value="radiobutton" />
+    <em>option3 from db</em></p>
+  <p>4. 
+    <input name="radiobutton" type="radio" value="radiobutton" />
+    <em>option4 from db</em></p>
+  <p>5. 
+    <input name="radiobutton" type="radio" value="radiobutton" />
+    <em>option5 from db</em></p>
+  <p>
+    <input type="submit" name="Submit" value="Vote" />
+</p>
+</form>
+<p>&nbsp; </p>
+</body>
+</html>
