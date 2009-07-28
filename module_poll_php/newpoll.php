962efc4a698af290b39b001e15f05b5ea61cb06a
mspatil1061 <mspatil1061@e549a66c-7351-11de-90dc-5d84f995b3ea>
2009-07-28 17:39:16 +0000

    php files for module-poll

---
+++ module_poll_php/newpoll.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>New Poll</title>
+</head>
+
+<body>
+<h2>Create New Poll</h2>
+<form id="form1" name="form1" method="post" action="">
+  <table width="200" border="1">
+    <tr>
+      <td><strong>Poll Question </strong></td>
+      <td><textarea name="pollquestion" id="pollquestion"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Option 1 </strong></td>
+      <td><textarea name="option1" id="option1"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Option 2 </strong></td>
+      <td><textarea name="option2" id="option2"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Option 3 </strong></td>
+      <td><textarea name="option3" id="option3"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Option 4 </strong></td>
+      <td><textarea name="option4" id="option4"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Option 5 </strong></td>
+      <td><textarea name="option5" id="option5"></textarea></td>
+    </tr>
+    <tr>
+      <td><strong>Expires on </strong></td>
+      <td><input name="expdate" type="text" id="expdate" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="Submit" value="Create Poll" /></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
---
+++ module_poll_php/viewpolls.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>View Polls</title>
+</head>
+
+<body>
+<table width="532" border="1" align="center">
+  <tr>
+    <td><strong>Poll Question </strong></td>
+    <td><strong>Created by </strong></td>
+    <td><strong>Votes so far </strong></td>
+    <td><strong>Closing Date </strong></td>
+  </tr>
+  <tr>
+    <td><a href="votepoll.php"><em>question from db </em></a></td>
+    <td><em>username from db </em></td>
+    <td><em>total votes from db </em></td>
+    <td><em>closing date from db </em></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p align="center"><a href="newpoll.php">Create New Poll </a> | <a href="#">View My Polls</a> </p>
+</body>
+</html>
